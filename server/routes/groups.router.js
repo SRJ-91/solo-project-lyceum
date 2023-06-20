97c4c7bf62c2c708f3857a183cc3c340e640356d
SRJ-91 <Sawyerjen@hotmail.com>
2023-06-20 17:44:42 -0500

    groups router PUT request(s) written and tested

--- server/routes/groups.router.js
+++ server/routes/groups.router.js
@@ -35,4 +35,116 @@ router.get('/fetch', (req, res) => {
     });
 });
 
+//ALL PUT REQUEST CODE BEYOND THIS POINT IS USED WITHIN THE DETAILS PAGE SCREEN
+
+// PUT Request to update group details
+router.put('/update/:id', (req, res) => {
+    const { id } = req.params;
+    const { region, book_name, team_name, cohort, start_date, end_date, cover, logo, details } = req.body;
+    const query = `UPDATE groups SET region = $1, book_name = $2, team_name = $3, cohort = $4, start_date = $5, end_date = $6,
+                   cover = $7, logo = $8, details = $9
+                   WHERE id = $10`;
+    const values = [region, book_name, team_name, cohort, start_date, end_date, cover, logo, details, id];
+  
+    pool.query(query, values)
+      .then(() => {
+        res.sendStatus(200);
+      })
+      .catch((error) => {
+        console.error('Error updating group:', error);
+        res.sendStatus(500);
+      });
+});
+
+module.exports = router;
+
+
+
+
+
+// // PUT Request to update majority of group data found in center center.
+// router.put('/update-header/:id', (req, res) => {
+//     const { id } = req.params;
+//     const { region, book_name, team_name, cohort, start_date, end_date } = req.body;
+//     const query = `UPDATE groups SET region = $1, book_name = $2, team_name = $3, cohort = $4, start_date = $5, end_date = $6
+//                    WHERE id = $7`;
+  
+//     pool.query(query, [region, book_name, team_name, cohort, start_date, end_date, id])
+//       .then(() => {
+//         res.sendStatus(200);
+//       })
+//       .catch((error) => {
+//         console.error('Error updating group details:', error);
+//         res.sendStatus(500);
+//       });
+// });
+
+// // PUT Request to update the cover
+// router.put('/update-cover/:id', (req, res) => {
+//     const { id } = req.params;
+//     const { cover } = req.body;
+//     const query = `UPDATE groups SET cover = $1
+//                    WHERE id = $2`;
+  
+//     pool.query(query, [cover, id])
+//       .then(() => {
+//         res.sendStatus(200);
+//       })
+//       .catch((error) => {
+//         console.error('Error updating group cover:', error);
+//         res.sendStatus(500);
+//       });
+// });
+
+// // PUT Request to update the logo
+// router.put('/update-logo/:id', (req, res) => {
+//     const { id } = req.params;
+//     const { logo } = req.body;
+//     const query = `UPDATE groups SET logo = $1
+//                    WHERE id = $2`;
+  
+//     pool.query(query, [logo, id])
+//       .then(() => {
+//         res.sendStatus(200);
+//       })
+//       .catch((error) => {
+//         console.error('Error updating group logo:', error);
+//         res.sendStatus(500);
+//       });
+// });
+
+// // PUT Request to update the details in the roster control/more info modal
+// router.put('/update-details-modal/:id', (req, res) => {
+//     const { id } = req.params;
+//     const { details } = req.body;
+//     const query = `UPDATE groups SET details = $1
+//                    WHERE id = $2`;
+  
+//     pool.query(query, [details, id])
+//       .then(() => {
+//         res.sendStatus(200);
+//       })
+//       .catch((error) => {
+//         console.error('Error updating group details in modal:', error);
+//         res.sendStatus(500);
+//       });
+// });
+
+// // PUT Request to update the status boolean to false (end group)
+// router.put('/end-group/:id', (req, res) => {
+//     const { id } = req.params;
+//     const query = `UPDATE groups SET status = false
+//                    WHERE id = $1`;
+  
+//     pool.query(query, [id])
+//       .then(() => {
+//         res.sendStatus(200);
+//       })
+//       .catch((error) => {
+//         console.error('Error ending group:', error);
+//         res.sendStatus(500);
+//       });
+// });
+
+
 module.exports = router;
