dcd7416b073a70698fab45dcae8dc594ba6e6096
SRJ-91 <Sawyerjen@hotmail.com>
2023-07-01 13:16:28 -0500

    Some styling done on badge view. Grids implemented there as well

--- src/components/Pages/AllBadges/AllBadges.jsx
+++ src/components/Pages/AllBadges/AllBadges.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import "./AllBadges.css";
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
-import {Button, ButtonGroup, Heading, Input, Text, Card, CardHeader, CardBody, CardFooter} from '@chakra-ui/react'
+import {Button, SimpleGrid, Img, ButtonGroup, Heading, Input, Text, Card, CardHeader, CardBody, CardFooter} from '@chakra-ui/react'
 
 const AllBadges = () => {
     const history = useHistory();
@@ -29,23 +29,28 @@ const AllBadges = () => {
     <Button onClick={() => history.push('/create-badge')}>Create a Badge</Button>
     </ButtonGroup>
     {/* Badges mapping */}
+    // <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
     {badges.map((badge) => (
-      <div className='badge' key={badge.id} >
-        <img 
+      
+      <Card variant={'elevated'} maxW={'md'} align={'center'} className='badge-card' key={badge.id} >
+        <CardBody>
+        <Img
+        className='badge-picture'
         src={badge.img} 
         alt="Badge"
         width={'200px'}
         onClick={() => handleBadgeClick(badge.id)}
         />
-        <div className='badge-description'>
-        <Text 
+        </CardBody>
+        <Text className='badge-name'>
+        <CardFooter
         color={'black'}>  
-          
         {badge.name}
+        </CardFooter>
         </Text>
-        </div>
-      </div>
+      </Card>
     ))}
+    </SimpleGrid>
   </div>
 );
 };
